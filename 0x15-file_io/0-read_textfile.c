c09c1cc28cff4a5ff31fa29f67b2b68fa410064f
Yaruda28 <yaredjemaneh91@gmail.com>
2023-05-09 01:05:11 -0700

    change

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,39 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <stdlib.h>
 
-ssize_t read_textfile(const char *filename, size_t letters) {
-    if (!filename) {
-        return 0;
-    }
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+  int fd;
+  ssize_t bytes_read = 0, bytes_written = 0;
+  char *buf;
 
-    int fd = open(filename, O_RDONLY);
-    if (fd == -1) {
-        return 0;
-    }
+  if (filename == NULL)
+    return (0);
 
-    char *buf = malloc(sizeof(char) * letters);
-    if (!buf) {
-        close(fd);
-        return 0;
-    }
+  fd = open(filename, O_RDONLY);
+  if (fd == -1)
+    return (0);
 
-    ssize_t bytes_read = read(fd, buf, letters);
-    if (bytes_read == -1) {
-        free(buf);
-        close(fd);
-        return 0;
-    }
+  buf = malloc(letters * sizeof(char));
+  if (buf == NULL)
+    return (0);
 
-    ssize_t bytes_written = write(STDOUT_FILENO, buf, bytes_read);
-    if (bytes_written == -1 || bytes_written != bytes_read) {
-        free(buf);
-        close(fd);
-        return 0;
-    }
+  bytes_read = read(fd, buf, letters);
+  if (bytes_read == -1)
+    return (0);
 
-    free(buf);
-    close(fd);
-    return bytes_read;
+  bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+  if (bytes_written == -1 || bytes_written != bytes_read)
+    return (0);
+
+  free(buf);
+  close(fd);
+
+  return (bytes_read);
 }
