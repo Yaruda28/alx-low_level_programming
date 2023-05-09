da06debfea77fc08829215e413a84a8861562c36
Yaruda28 <yaredjemaneh91@gmail.com>
2023-05-09 07:07:53 -0700

    ya

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,40 +1,34 @@
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdlib.h>
+#include "main.h"
 
 /**
- * create_file - creates a file with the specified permissions and writes a
- *               string to it, or creates an empty file if no string is provided
- * @filename: the name of the file to create
- * @text_content: a NULL terminated string to write to the file, or NULL to
- *                create an empty file
+ * create_file - Created the file.
+ * @filename: the pointer to the name of the files created.
+ * @text_content: A pointer to a string to writes a files.
  *
- * Return: 1 on success, -1 on failure (file can not be created, file can not
- *         be written, write "fails", etc...)
+ * Return: when a function is failed - -1.
+ *         Otherwise - 1.
  */
 int create_file(const char *filename, char *text_content)
 {
-  int fd, result, text_len = 0;
+	int yd, x, bnm = 0;
 
-  if (filename == NULL)
-    return (-1);
+	if (filename == NULL)
+		return (-1);
 
-  if (text_content != NULL) {
-    while (text_content[text_len] != '\0')
-      text_len++;
-  }
+	if (text_content != NULL)
+	{
+		for (bmn = 0; text_content[bnm];)
+			bnm++;
+	}
 
-  fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
-  if (fd == -1)
-    return (-1);
+	yd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	x = write(yd, text_content, bnm);
 
-  if (text_len > 0) {
-    result = write(fd, text_content, text_len);
-    if (result == -1)
-      return (-1);
-  }
+	if (yd == -1 || x == -1)
+		return (-1);
 
-  close(fd);
+	close(yd);
 
-  return (1);
+	return (1);
 }
+
