b6c30f03debc3d4c7138e6a06d08f4065a012577
Yaruda28 <yaredjemaneh91@gmail.com>
2023-05-09 01:16:19 -0700

    3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#define BUFSIZE 1024
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: the number of arguments
+ * @argv: the array of arguments
+ *
+ * Return: 0 on success, or an error code if an error occurred
+ */
+int main(int argc, char *argv[])
+{
+  int fd_from, fd_to, result;
+  char buffer[BUFSIZE];
+  ssize_t bytes_read, bytes_written;
+
+  if (argc != 3) {
+    dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+    exit(97);
+  }
+
+  fd_from = open(argv[1], O_RDONLY);
+  if (fd_from == -1) {
+    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+    exit(98);
+  }
+
+  fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+  if (fd_to == -1) {
+    dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+    exit(99);
+  }
+
+  while ((bytes_read = read(fd_from, buffer, BUFSIZE)) > 0) {
+    bytes_written = write(fd_to, buffer, bytes_read);
+    if (bytes_written != bytes_read) {
+      dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+      close(fd_from);
+      close(fd_to);
+      exit(99);
+    }
+  }
+
+  if (bytes_read == -1) {
+    dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+    close(fd_from);
+    close(fd_to);
+    exit(98);
+  }
+
+  result = close(fd_from);
+  if (result == -1) {
+    dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+    exit(100);
+  }
+
+  result = close(fd_to);
+  if (result == -1) {
+    dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+    exit(100);
+  }
+
+  return (0);
+}
